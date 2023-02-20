6a5b144f48647519174ca285c8bf45cb887a8370
JoJo <89042174+JoJoDevAdventure@users.noreply.github.com>
2023-02-20 20:38:52 +0100

    commit

--- afa_django/product/urls.py
+++ afa_django/product/urls.py
@@ -1,6 +1,11 @@
-from django.urls import path, include
+from django.urls import include, path
 from product import views
 
 urlpatterns = [
     path('latest-products/', views.LatestProductsList.as_view()),
+    path('latest-collections/', views.LatestCollectionsList.as_view()),
+    path ('products/<slug:collection_slug>/<slug:product_slug>/', views.ProductDetail.as_view()),
+    path ('collections/<slug:collection_slug>/', views.CollectionDetails.as_view()),
+    path ('collections/<slug:collection_slug>/all', views.CollectionProducts.as_view()),
+    path('collections/', views.CollectionsList.as_view()),
     ]
