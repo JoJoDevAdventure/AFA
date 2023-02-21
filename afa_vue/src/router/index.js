cfc53f1662ea3f3f741ba5c7a3895c99addbb8f9
JoJo <89042174+JoJoDevAdventure@users.noreply.github.com>
2023-02-21 09:56:19 +0100

    + Componment

--- afa_vue/src/router/index.js
+++ afa_vue/src/router/index.js
@@ -1,16 +1,24 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import AboutView from '../views/AboutView.vue'
 import CartView from '../views/CartView.vue'
+import CheckoutView from '../views/CheckoutView.vue'
 import CollectionDetailsView from '../views/CollectionDetailsView.vue'
 import CollectionsView from '../views/CollectionsView.vue'
 import ContactView from '../views/ContactView.vue'
 import HomeView from '../views/HomeView.vue'
 import ProductView from '../views/ProductView.vue'
+import SuccessView from '../views/SuccessView.vue'
 
 const routes = [
   {
-    path: '/home',
+    path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: AboutView
   },
   {
     path: '/collections/',
@@ -37,6 +45,18 @@ const routes = [
     path: '/cart',
     name: 'cart',
     component: CartView,
+  },
+
+  {
+    path: '/cart/checkout',
+    name: 'checkout',
+    component: CheckoutView,
+  },
+
+  {
+    path: '/cart/success',
+    name: 'success',
+    component: SuccessView,
   },
 ]
 
