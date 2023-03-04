395bf37aabd7a76c42c4d56ff85e5e11ba9c7e08
JoJo <89042174+JoJoDevAdventure@users.noreply.github.com>
2023-03-04 15:18:36 +0100

    Success page view

---
+++ afa_vue/src/views/SuccessView.vue
@@ -0,0 +1,25 @@
+<template>
+    <div id="success">
+        <div>
+            <h1>Your order is passed!</h1>
+            <h1>We'll call you soon 🙃</h1>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+#success {
+    height: 20vh;
+    justify-content: center;
+    justify-items: center;
+    margin-top: 150px;
+    margin-bottom: 100px;
+}
+h1 {
+    color: white;
+
+    font-size: 52px;
+    text-align: center;
+}
+
+</style>
